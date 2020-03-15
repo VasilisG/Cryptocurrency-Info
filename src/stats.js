01b1680f45bb60395db40e188dcbc00d32fb85fb
Vasilis <vasilisgoutz08@gmail.com>
2020-03-15 12:04:11 +0200

    Committing project.

---
+++ src/stats.js
@@ -0,0 +1,53 @@
+$.fn.fetchStats = function(){
+    var $statsArea = $(".stats-area");
+    var $totalCoins = $statsArea.find(".total-coins");
+    var $totalMarkets = $statsArea.find(".total-markets");
+    var $totalTradingVolume = $statsArea.find(".total-trading-volume");
+    var $totalExchanges = $statsArea.find(".total-exchanges");
+    var $totalMarketCap = $statsArea.find(".total-market-cap");
+
+    var $statsAreaError = $(".stats-area-error");
+    var $errorCode = $statsAreaError.children('.code');
+    var $messageCode = $statsAreaError.children('.message');
+
+    var currency = $(".currency-dropdown option:selected").text();
+    var symbol = currency == 'EUR' ? '€' : '$';
+
+    $.ajax({
+        url : 'https://api.coinranking.com/v1/public/stats?' + currency,
+        success: function(result, status, jqXHR){  
+            if(result.status === 'success'){
+                $totalCoins.siblings("p").html(result.data.totalCoins);
+                $totalMarkets.siblings("p").html(result.data.totalMarkets);
+                $totalTradingVolume.siblings("p").html(result.data.total24hVolume.toLocaleString() + "<sup>" + symbol + "</sup>");
+                $totalExchanges.siblings("p").html(result.data.totalExchanges);
+                $totalMarketCap.siblings("p").html(result.data.totalMarketCap.toLocaleString() + "<sup>" + symbol + "</sup>");
+                $statsArea.addClass("active");
+            } 
+            else {
+                $errorCode.text(result.code);
+                $messageCode.text(result.message);
+                $statsAreaError.addClass("active");
+            }
+        },
+        error: function(xhr, status, error){
+            $statsAreaError.html(status + " " + error);
+        }
+    });
+}
+
+$.fn.getStats = function(interval){
+    $("body").fetchStats();
+    setInterval(function(){
+        $("body").fetchStats();  
+    },interval);
+}
+
+$(document).ready(function(){
+    $(".currency-dropdown option").val('EUR');
+    $.fn.fetchStats();
+
+    $(".refresh").on("click", function(){
+        $.fn.fetchStats();
+    });
+});
