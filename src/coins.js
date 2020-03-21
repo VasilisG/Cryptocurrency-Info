7075c6e5697777bc811ad331da94b7ef2a394801
Vasilis <vasilisgoutz08@gmail.com>
2020-03-21 16:50:05 +0200

    Added coin list items.

---
+++ src/coins.js
@@ -0,0 +1,20 @@
+$.fn.fetchCoins = function(){
+
+    var $errorArea = $(".error-area");
+    var $errorCode = $errorArea.children('.code');
+    var $messageCode = $errorArea.children('.message');
+
+    $.ajax({
+        url : 'https://api.coinranking.com/v1/public/coins',
+        success: function(result, status, jqXHR){
+            console.log(result);
+        },
+        error: function(xhr, status, error){
+            $errorArea.html(status + " " + error);
+        }
+    });
+}
+
+$(document).ready(function(){
+    $.fn.fetchCoins();
+});
