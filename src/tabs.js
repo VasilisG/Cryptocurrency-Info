047fc947d4c1cd41b8fbfcfa3536bc3b8be9c041
Vasilis <vasilisgoutz08@gmail.com>
2020-03-23 20:53:27 +0200

    Removed coin history and get coin item tab.

--- src/tabs.js
+++ src/tabs.js
@@ -4,15 +4,11 @@ $(document).ready(function(){
     var $coinsItem = $('.coins-item');
     var $marketsItem = $('.markets-item');
     var $exchangesItem = $('.exchanges-item');
-    var $getCoinItem = $('.get-coin-item');
-    var $getCoinHistoryItem = $('.get-coin-history-item');
 
     var $statsArea = $('.stats-area');
     var $coinsArea = $('.coins-area');
     var $marketsArea = $('.markets-area');
     var $exchangesArea = $('.exchanges-area');
-    var $getCoinArea = $('.get-coin-area');
-    var $getCoinHistoryArea = $('.get-coin-history-area');
 
     $statsItem.addClass('active-tab');
 
@@ -25,10 +21,6 @@ $(document).ready(function(){
         $marketsArea.removeClass('active');
         $exchangesItem.removeClass('active-tab');
         $exchangesArea.removeClass('active');
-        $getCoinItem.removeClass('active-tab');
-        $getCoinArea.removeClass('active');
-        $getCoinHistoryItem.removeClass('active-tab');
-        $getCoinHistoryArea.removeClass('active');
     });
 
     $coinsItem.click(function(){
@@ -40,10 +32,6 @@ $(document).ready(function(){
         $marketsArea.removeClass('active');
         $exchangesItem.removeClass('active-tab');
         $exchangesArea.removeClass('active');
-        $getCoinItem.removeClass('active-tab');
-        $getCoinArea.removeClass('active');
-        $getCoinHistoryItem.removeClass('active-tab');
-        $getCoinHistoryArea.removeClass('active');
     });
 
     $marketsItem.click(function(){
@@ -55,10 +43,6 @@ $(document).ready(function(){
         $marketsArea.addClass('active');
         $exchangesItem.removeClass('active-tab');
         $exchangesArea.removeClass('active');
-        $getCoinItem.removeClass('active-tab');
-        $getCoinArea.removeClass('active');
-        $getCoinHistoryItem.removeClass('active-tab');
-        $getCoinHistoryArea.removeClass('active');
     });
 
     $exchangesItem.click(function(){
@@ -70,39 +54,5 @@ $(document).ready(function(){
         $marketsArea.removeClass('active');
         $exchangesItem.addClass('active-tab');
         $exchangesArea.addClass('active');
-        $getCoinItem.removeClass('active-tab');
-        $getCoinArea.removeClass('active');
-        $getCoinHistoryItem.removeClass('active-tab');
-        $getCoinHistoryArea.removeClass('active');
-    });
-
-    $getCoinItem.click(function(){
-        $statsItem.removeClass('active-tab');
-        $statsArea.removeClass('active');
-        $coinsItem.removeClass('active-tab');
-        $coinsArea.removeClass('active');
-        $marketsItem.removeClass('active-tab');
-        $marketsArea.removeClass('active');
-        $exchangesItem.removeClass('active-tab');
-        $exchangesArea.removeClass('active');
-        $getCoinItem.addClass('active-tab');
-        $getCoinArea.addClass('active');
-        $getCoinHistoryItem.removeClass('active-tab');
-        $getCoinHistoryArea.removeClass('active');
-    });
-
-    $getCoinHistoryItem.click(function(){
-        $statsItem.removeClass('active-tab');
-        $statsArea.removeClass('active');
-        $coinsItem.removeClass('active-tab');
-        $coinsArea.removeClass('active');
-        $marketsItem.removeClass('active-tab');
-        $marketsArea.removeClass('active');
-        $exchangesItem.removeClass('active-tab');
-        $exchangesArea.removeClass('active');
-        $getCoinItem.removeClass('active-tab');
-        $getCoinArea.removeClass('active');
-        $getCoinHistoryItem.addClass('active-tab');
-        $getCoinHistoryArea.addClass('active');
     });
 });
