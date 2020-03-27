83544b64a93a4ed55d3e982917a36452088c9063
Vasilis <vasilisgoutz08@gmail.com>
2020-03-28 01:05:14 +0200

    Coins are loading.

--- src/coins.js
+++ src/coins.js
@@ -9,38 +9,46 @@ $.fn.fetchCoins = function(){
     $.ajax({
         url : 'https://api.coinranking.com/v1/public/coins',
         success: function(data, status, jqXHR){
+
             console.log(data);
             console.log(data['status']);
-            if(data['status'] === 'Success'){
+
+            if(data['status'] === 'success'){
+
+                var coins = data['data']['coins'];
+
+                var $coinItem, $coinBasicInfo, $coinMoreInfo;
+                var $coinName, $coinSymbol, $coinIcon, $coinPrice, $coinChange, $coinAction;
+                var $coinDescription, $coinVolume, $coinMarketGap, $coinCirculatingSupply, $coinTotalSupply,
+                    $coinFirstSeen, $coinNumberOfMarkets, $coinNumberOfExchanges, $coinHighestPrice, $coinHighestPriceDate;
+
                 $coinList.empty();
-                var coins = data['coins'];
+
                 $.each(coins, function(index, coinData){
-                    var $coinItem = $('<div class="coin-list-item"></div>');
-                    var $coinBasicInfo = $('<div class="basic-info"></div>');
-                    var $coinMoreInfo = $('<div class="more-info"></div>');
 
-                    var $coinName = $('<div class="name">' + coinData['name'] + "</div>");
-                    var $coinSymbol = $('<div class="symbol">' + coinData['symbol'] + "</div>");
-                    var $coinIcon = $('<div class="icon">' + '<img src="' + coinData['iconUrl'] + '"/></div>');
-                    var $coinPrice = $('<div class="price">' + coinData['price'] + '</div>');
-                    var $coinChange = $('<div class="change">' + coinData['change'] + '</div>');
-                    var $coinAction = $('<div class="more">More</div>');
+                    $coinItem = $('<div class="coin-list-item"></div>');
+                    $coinBasicInfo = $('<div class="basic-info"></div>');
+                    $coinMoreInfo = $('<div class="more-info"></div>');
 
-                    var $coinDescription = $('<div class="description"><p class="more-info-title">Description:</p><p class="more-info-data">' + coinData['description'] +'</p></div>');
-                    var $coinVolume = $('<div class="volume"><p class="more-info-title">Volume:</p><p class="more-info-data"' + coinData['volume'] + '</p></div>');
-                    var $coinMarketGap = $('<div class="marketCap"><p class="more-info-title">Market Cap:</p><p class="more-info-data">' + coinData['marketCap'] + '</p></div>');
-                    var $coinCirculatingSupply = $('<div class="circulating-supply"><p class="more-info-title">Circulating supply:</p><p class="more-info-data">' + coinData['circulatingSupply'] + '</p></div>');
-                    var $coinTotalSupply = $('<div class="total-supply"><p class="more-info-title">Total supply:</p><p class="more-info-data">' + coinData['totalSupply'] + '</p></div>');
-                    var $coinFirstSeen = $('<div class="first-seen"><p class="more-info-title">First seen:</p><p class="more-info-data">' + coinData['firstSeen'] + '</p></div>');
-                    var $coinNumberOfMarkets = $('<div class="number-of-markets"><p class="more-info-title">Number of markets:</p><p class="more-info-data">' + coinData['numberOfMarkets'] + '</p></div>');
-                    var $coinNumberOfExchanges = $('<div class="number-of-exchanges"><p class="more-info-title">Number of exchanges:</p><p class="more-info-data">' + coinData['numberOfExchanges'] + '</p></div>');
-                    var $coinHighestPrice = $('<div class="highest-price"><p class="more-info-title">Highest price:</p><p class="more-info-data">' + coinData['allTimeHigh']['price'] + '</p></div>');
-                    var $coinHighstPriceDate = $('<div class="highest-price-date"><p class="more-info-title">Highest price data:</p><p class="more-info-data">' + coinData['allTimeHigh']['timestamp'] + '</p></div>');
+                    $coinName = $('<div class="name">' + coinData['name'] + '</div>');
+                    $coinSymbol = $('<div class="symbol">' + coinData['symbol'] + '</div>');
+                    $coinIcon = $('<div class="icon">' + '<img src="' + coinData['iconUrl'] + '"/></div>');
+                    $coinPrice = $('<div class="price">' + coinData['price'] + '</div>');
+                    $coinChange = $('<div class="change">' + coinData['change'] + '</div>');
+                    $coinAction = $('<div class="more">More</div>');
 
-                    $coinList.append($coinItem);
-                    $coinItem.append($coinBasicInfo);
-                    $coinItem.append($coinMoreInfo);
+                    $coinDescription = $('<div class="description"><p class="more-info-title">Description:</p><p class="more-info-data">' + coinData['description'] +'</p></div>');
+                    $coinVolume = $('<div class="volume"><p class="more-info-title">Volume:</p><p class="more-info-data"' + coinData['volume'] + '</p></div>');
+                    $coinMarketGap = $('<div class="marketCap"><p class="more-info-title">Market Cap:</p><p class="more-info-data">' + coinData['marketCap'] + '</p></div>');
+                    $coinCirculatingSupply = $('<div class="circulating-supply"><p class="more-info-title">Circulating supply:</p><p class="more-info-data">' + coinData['circulatingSupply'] + '</p></div>');
+                    $coinTotalSupply = $('<div class="total-supply"><p class="more-info-title">Total supply:</p><p class="more-info-data">' + coinData['totalSupply'] + '</p></div>');
+                    $coinFirstSeen = $('<div class="first-seen"><p class="more-info-title">First seen:</p><p class="more-info-data">' + coinData['firstSeen'] + '</p></div>');
+                    $coinNumberOfMarkets = $('<div class="number-of-markets"><p class="more-info-title">Number of markets:</p><p class="more-info-data">' + coinData['numberOfMarkets'] + '</p></div>');
+                    $coinNumberOfExchanges = $('<div class="number-of-exchanges"><p class="more-info-title">Number of exchanges:</p><p class="more-info-data">' + coinData['numberOfExchanges'] + '</p></div>');
+                    $coinHighestPrice = $('<div class="highest-price"><p class="more-info-title">Highest price:</p><p class="more-info-data">' + coinData['allTimeHigh']['price'] + '</p></div>');
+                    $coinHighestPriceDate = $('<div class="highest-price-date"><p class="more-info-title">Highest price data:</p><p class="more-info-data">' + coinData['allTimeHigh']['timestamp'] + '</p></div>');
 
+                    
                     $coinBasicInfo.append($coinName);
                     $coinBasicInfo.append($coinSymbol);
                     $coinBasicInfo.append($coinIcon);
@@ -57,7 +65,12 @@ $.fn.fetchCoins = function(){
                     $coinMoreInfo.append($coinNumberOfMarkets);
                     $coinMoreInfo.append($coinNumberOfExchanges);
                     $coinMoreInfo.append($coinHighestPrice);
-                    $coinMoreInfo.append($coinHighstPriceDate);
+                    $coinMoreInfo.append($coinHighestPriceDate);
+
+                    $coinItem.append($coinBasicInfo);
+                    $coinItem.append($coinMoreInfo);
+
+                    $coinList.append($coinItem);
                 });
             }
             else{
@@ -78,7 +91,6 @@ $(document).ready(function(){
         if($moreInfo.hasClass('more-info-active')){
             $moreInfo.removeClass('more-info-active');
         }
-        else $moreInfo.addClass('more-info-active');
-        // $(this).parent().siblings('.more-info').slideToggle();
+        else $moreInfo.addClass('more-info-active');;
     });
 });
