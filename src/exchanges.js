1b22a3e7ea03c9efa6387a8482e436ee326be7a4
Vasilis <vasilisgoutz08@gmail.com>
2020-05-02 15:56:35 +0300

    Created exchanges tab.

--- src/exchanges.js
+++ src/exchanges.js
@@ -4,3 +4,133 @@ var currentExchangePage = 0;
 function getQuantityElement(elem){
     return elem === null ? '-' :  elem.toLocaleString();
 }
+
+$.fn.fetchExchanges = function(page){
+
+    var $errorArea = $(".error-area");
+    var $exchangeList = $('.exchange-list');
+    var $exchangeLoadingOverlay = $('.exchange-loading-overlay');
+
+    var sorting = '?order=' + $('.exchange-sort-selector .sort-dropdown option:selected').val();
+    var order = '&orderDirection=' + $('.exchange-order-selector .order-dropdown option:selected').val();
+    var limit = '&limit=100';
+    var offset = '&offset=' + page * pageExchangeSize;
+
+    $exchangeList.addClass('stand-by');
+    $exchangeLoadingOverlay.addClass('overlay-active');
+
+    $.ajax({
+        url : 'https://api.coinranking.com/v1/public/exchanges' + sorting + order + limit + offset,
+        success: function(data, status, jqXHR){
+
+            $exchangeList.removeClass('stand-by');
+            $exchangeLoadingOverlay.removeClass('overlay-active');
+
+            if(data['status'] == 'success'){
+
+                var stats = data['data']['stats']
+                var totalExchanges = stats['total'];
+                var totalExchangePages = parseInt(totalExchanges / pageExchangeSize) + 1;
+
+                var exchanges = data['data']['exchanges'];
+
+                var $sourceName, $sourceIcon, $sourceVolume, $sourcePrice, $sourceMarketShare;
+                var $exchangeItem, $exchangeBasicInfo;
+
+                $('.exchange-pagination #exchange-current-page').val(page + 1);
+                $('.exchange-pagination .total-pages').text(totalExchangePages);
+
+                $('.exchange-list-item').remove();
+
+                $.each(exchanges, function(index, exchangeData){
+
+                    $exchangeItem = $('<div class="exchange-list-item"></div>');
+                    $exchangeBasicInfo = $('<div class="basic-info"></div>');
+
+                    $exchangeName = $('<div class="exchange-name"><span>' + exchangeData['name'] + '</span></div>');
+                    $exchangeIcon = $('<div class="exchange-icon"><img src="' + exchangeData['iconUrl'] + '"/></div>');
+                    $exchangeVolume = $('<div class="exchange-volume"><span>' + getQuantityElement(exchangeData['volume']) + '</span></div>');
+                    $exchangePrice = $('<div class="exchange-number-of-markets"><span>' + exchangeData['numberOfMarkets'] + '</span></div>');
+                    $exchangeMarketShare = $('<div class="exchange-market-share"><span>' + getQuantityElement(exchangeData['marketShare']) + '</span></div>');
+                    $exchangeWebsite = $('<div class="exchange-website"><a href="' + exchangeData['websiteUrl'] + '" target="_blank"><span>Visit Website<i class="fa fas fa-link"></i></span></a></div>');
+                    
+
+                    $exchangeBasicInfo.append($exchangeName);
+                    $exchangeBasicInfo.append($exchangeIcon);
+                    $exchangeBasicInfo.append($exchangeVolume);
+                    $exchangeBasicInfo.append($exchangePrice);
+                    $exchangeBasicInfo.append($exchangeMarketShare);
+                    $exchangeBasicInfo.append($exchangeWebsite);
+
+                    $exchangeItem.append($exchangeBasicInfo);
+                    $exchangeList.append($exchangeItem);
+                });
+            }
+            else {
+
+            }
+        },
+        error: function(xhr, status, error){
+            $errorArea.html(status + " " + error);
+        }
+    });
+}
+
+$.fn.attachExchangeRefreshButtonListener = function() {
+    $('.exchange-refresh').on('click', function(){
+        var currentPage = parseInt($('#exchange-current-page').val());
+        currentExchangePage = currentPage - 1;
+        $.fn.fetchExchanges(currentExchangePage);
+    });
+}
+
+$.fn.attachExchangePrevPageListener = function() {
+    $('.exchange-pagination .prev').on('click', function(){
+        var currentPage = parseInt($('#exchange-current-page').val());
+        if(currentPage > 1) {
+            currentPage--;
+            currentExchangePage = currentPage - 1;
+            $.fn.fetchExchanges(currentExchangePage);
+        }
+    });
+}
+
+$.fn.attachExchangeNextPageListener = function() {
+    $('.exchange-pagination .next').on('click', function(){
+        var currentPage = parseInt($('#exchange-current-page').val());
+        var totalExchangePages = parseInt($('.total-pages').text());
+        if(currentPage < totalExchangePages){
+            currentPage++;
+            currentExchangePage = currentPage - 1;
+            $.fn.fetchExchanges(currentExchangePage);
+        }
+    });
+}
+
+$.fn.attachExchangeKeyListener = function() {
+    $('#exchange-current-page').keypress(function(event){
+        if(event.which == 13){
+            var totalExchangePages = parseInt($('.total-pages').text());
+            var fieldData = $(this).val();
+            var isNumber = /^\d+$/.test(fieldData);
+            if(isNumber){
+                var pageNumber = parseInt(fieldData);
+                if(pageNumber < 1 || pageNumber > totalExchangePages){
+                    $(this).val(currentExchangePage+1);
+                }
+                else {
+                    currentExchangePage = $(this).val()-1;
+                    $.fn.fetchExchanges(currentExchangePage);
+                }
+            }
+        }
+    });
+}
+
+$(document).ready(function(){
+    $.fn.fetchExchanges(currentExchangePage);
+    $.fn.attachExchangeRefreshButtonListener(); 
+    $.fn.attachExchangePrevPageListener();
+    $.fn.attachExchangeNextPageListener();  
+    $.fn.attachExchangeKeyListener(); 
+});
